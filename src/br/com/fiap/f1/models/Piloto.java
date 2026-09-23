14e77469d248ff4dfc40d2a68dff146c7bc40281
Guilherme <guimmalmeidaads@gmail.com>
2026-09-22 21:22:32 -0300

    feat: organiza packages e cria a classe Piloto

---
+++ src/br/com/fiap/f1/models/Piloto.java
@@ -0,0 +1,11 @@
+package br.com.fiap.f1.models;
+
+public class Piloto {
+    //ATRIBUTOS
+    private int id;
+    private String nome;
+    private int idade;
+    private String equipe;
+    private String nacionalidade;
+    private double pontos;
+}
