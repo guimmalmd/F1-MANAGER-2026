04bdeb1403489ea3dc43856b49720e25ad67370e
Guilherme <guimmalmeidaads@gmail.com>
2026-09-24 16:23:28 -0300

    fix: arruma classe piloto

--- src/br/com/fiap/f1/models/Piloto.java
+++ src/br/com/fiap/f1/models/Piloto.java
@@ -1,12 +1,59 @@
 package br.com.fiap.f1.models;
 
 public class Piloto {
-    //ATRIBUTOS
-    // Private quer dizer que esse atributo não deve ser acessado diretamente por qualquer classe.
-    private int id; // id é diferente do número do piloto
+
     private String nome;
     private int numero;
+    private String equipe;
     private String nacionalidade;
     private double pontos;
-    private Equipe equipe;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getEquipe() {
+        return equipe;
+    }
+
+    public void setEquipe(String equipe) {
+        this.equipe = equipe;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public double getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(double pontos) {
+        this.pontos = pontos;
+    }
+
+    @Override
+    public String toString() {
+        return "Piloto: " + nome +
+                " | Nº " + numero +
+                " | Equipe: " + equipe +
+                " | Nacionalidade: " + nacionalidade +
+                " | Pontos: " + pontos;
+    }
 }
